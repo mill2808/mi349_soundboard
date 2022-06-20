5d5269c60a6bd98cf2bac4eba15b5fdacf448eea
mill2808 <105799175+mill2808@users.noreply.github.com>
2022-06-19 23:42:33 -0400

    added a noise

--- soundboard.js
+++ soundboard.js
@@ -1,33 +1,36 @@
 
 
 var sound1 = new Audio("boing_x.wav");
+var sound4 = new Audio("boing_poing.wav");
 var buttonElement1 = document.getElementById('boing')
 buttonElement1.addEventListener('click', function() {
     sound1.play()
 })
 buttonElement1.addEventListener('mouseenter', function() {
-    sound1.play()
+    sound4.play()
 })
 
 
 
 var sound2 = new Audio("can_pop.wav");
+var sound5 = new Audio("cork_pop_x.wav");
 var buttonElement2 = document.getElementById('pop')
 buttonElement2.addEventListener('click', function() {
     sound2.play()
 })
 buttonElement2.addEventListener('mouseenter', function() {
-    sound2.play()
+    sound5.play()
 })
 
 
 var sound3 = new Audio("bomb_x.mp3.wav");
+var sound6 = new Audio("croc_chomp_x.wav");
 var buttonElement3 = document.getElementById('boom')
 buttonElement3.addEventListener('click', function() {
     sound3.play()
 })
 buttonElement3.addEventListener('mouseenter', function() {
-    sound3.play()
+    sound6.play()
 })
 
 
