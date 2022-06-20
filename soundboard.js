d9e3e5734af11e996cfc1fb66155df282a49a9a4
mill2808 <105799175+mill2808@users.noreply.github.com>
2022-06-19 23:44:37 -0400

    validation fix

--- soundboard.js
+++ soundboard.js
@@ -1,4 +1,11 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Javascript</title>
+</head>
 
 var sound1 = new Audio("boing_x.wav");
 var sound4 = new Audio("boing_poing.wav");
